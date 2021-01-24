c55be6e6fd3244d2d3e100cd7ae53eec3a0ab8e8
Dominik Bachorz <bachorzdominik.mail+github@gmail.com>
2021-01-24 21:46:56 +0100

    Project: Loan Calculator, Stage 3/4

--- JetBrains-Academy/Loan Calculator/Loan Calculator/task/creditcalc/creditcalc.py
+++ JetBrains-Academy/Loan Calculator/Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,20 +1,50 @@
-# https://hyperskill.org/projects/90/stages/501/implement
-from math import ceil
+# https://hyperskill.org/projects/90/stages/502/implement
+import math
 
-loan_principal = int(input('Enter the loan principal: '))
 calculate = input('''What do you want to calculate?
-type "m" - for number of monthly payments,
-type "p" - for the monthly payment:''')
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:
+''')
 
-if calculate == 'm':
+if calculate == 'n':
+    loan_principal = int(input('Enter the loan principal: '))
     monthly_payment = int(input('Enter the monthly payment: '))
-    n_months = ceil(loan_principal / monthly_payment)
-    if n_months == 1:
-        print(f'It will take {n_months} month to repay the loan')
+    loan_interest = float(input('Enter the loan interest: '))
+
+    interest_rate = (loan_interest / 100) / (12 * 1)
+
+    number_of_months = math.log((monthly_payment / (monthly_payment - interest_rate * loan_principal)),
+                                (1 + interest_rate))
+    number_of_months = math.ceil(number_of_months)
+
+    if number_of_months == 1:
+        print(f'It will take {number_of_months} month to repay the loan')
+    elif number_of_months <= 12:
+        print(f'It will take {number_of_months} months to repay the loan')
     else:
-        print(f'It will take {n_months} months to repay the loan')
+        years = math.floor(number_of_months / 12)
+        months = number_of_months - (12 * years)
+        print(f'It will take {years} years and {months} months to repay this loan!')
+
+elif calculate == 'a':
+    loan_principal = int(input('Enter the loan principal: '))
+    number_of_months = int(input('Enter the number of periods: '))
+    loan_interest = float(input('Enter the loan interest: '))
+
+    interest_rate = (loan_interest / 100) / (12 * 1)
+
+    monthly_payment = loan_principal * ((interest_rate * pow(1 + interest_rate, number_of_months))
+                                        / (pow(1 + interest_rate, number_of_months) - 1))
+    print(f'Your monthly payment = {math.ceil(monthly_payment)}!')
+
 elif calculate == 'p':
-    n_months = int(input('Enter the number of months: '))
-    payment = ceil(loan_principal / n_months)
-    last_payment = loan_principal - (n_months - 1) * payment
-    print(f'Your monthly payment = {payment} and the last payment {last_payment}')
+    annuity_payment = float(input('Enter the annuity payment: '))
+    number_of_months = int(input('Enter the number of periods: '))
+    loan_interest = float(input('Enter the loan interest: '))
+
+    interest_rate = (loan_interest / 100) / (12 * 1)
+    loan_principal = annuity_payment / ((interest_rate * math.pow(1 + interest_rate, number_of_months))
+                                        / (math.pow(1 + interest_rate, number_of_months) - 1))
+
+    print(f'Your loan principal = {round(loan_principal)}!')
